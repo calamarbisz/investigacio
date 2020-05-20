e5979b3ba41b799e0a8188fca44509a7acecb1bd
calamarbisz <60406035+calamarbisz@users.noreply.github.com>
2020-05-20 22:35:41 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let img
+		let value=0
     function preload() {
       img = loadImage('room.png');
     }
@@ -8,18 +9,22 @@ let img
       background(img, windowWidth,windowHeight);
     }
 
+function mouseClicked(event) {
+  console.log(event);
+}
+
     let index = 0
     function draw() {
-      background(img, windowWidth,windowHeight);
+      //background();
       divide(0, 0, windowWidth, windowHeight, 6, 0, 0, windowWidth, windowHeight)
     }
 
     function divide(x, y, w, h, depth, ix, iy, iw, ih) {
       if (depth > 0) {
-        const n = noise(w / width * 2, w / height * 2, frameCount / 800 * depth)
-        if (depth-- % 2 === 1) {
+        const n = noise(w / width * 1, w / height * 2, frameCount / 2000 * depth)
+        if (depth-- % 3 === 1) {
           divide(x, y, w * n, h, depth,
-            ix, iy, iw / 2, ih)
+            ix, iy, iw / 5, ih)
           divide(x + w * n, y, w - w * n, h, depth,
             ix + iw / 2, iy, iw / 2, ih)
         } else {
@@ -30,11 +35,13 @@ let img
             ix, iy + ih / 2, iw, ih / 2
           )
         }
-      } else {
+      } else if (mouseClicked) {
         push()
         noFill()
         image(img, x, y, w, h, ix, iy, iw, ih)
+        noStroke()
+        rect(x, y, w, h)
         pop()
         //text(index, x, y + 10)
-      }
+      } 
     }
